e8edec7ea5e99c44a2aaf31512cbe82387297f14
Shivam Rathore <shivamrathore1126@gmail.com>
2026-05-04 17:37:58 +0530

    operators in python

---
+++ 04_operators.py
@@ -0,0 +1,49 @@
+# operators are symbols used to do operations on values
+
+# arithmetic operators (math work)
+
+A = 10
+B = 5
+
+print(A + B)   # add
+print(A - B)   # subtract
+print(A * B)   # multiply
+print(A / B)   # divide
+print(A % B)   # remainder
+print(A ** B)  # power
+print(A // B)  # floor division
+
+
+# assignment operators (give value)
+
+A = 10
+A += 5   # add and assign
+print(A)
+
+A -= 2   # subtract and assign
+print(A)
+
+
+# comparison operators (check true or false)
+
+A = 10
+B = 5
+
+print(A == B)   # equal or not
+print(A != B)   # not equal
+print(A > B)    # greater
+print(A < B)    # smaller
+print(A >= B)   # greater or equal
+print(A <= B)   # smaller or equal
+
+
+# logical operators (use with conditions)
+
+A = True
+B = False
+
+print(A and B)   # both true
+print(A or B)    # one true
+print(not A)     # reverse
+
+
