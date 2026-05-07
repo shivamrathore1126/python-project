a5ac75024ed4eebc50cba948fbf5644b4c92125a
Shivam Rathore <shivamrathore1126@gmail.com>
2026-05-07 15:19:44 +0530

    user input in python

---
+++ 06_user_input.py
@@ -0,0 +1,38 @@
+
+
+a = input("enter your name:")
+print("my name is ",a)
+
+
+x = input("enter first number:")
+y = input ("enter second number:")
+print(x + y)
+print(int(x)+int(y))
+
+# float
+x = input("enter first number:")
+y = input ("enter second number:")
+print(float(x)+float(y))
+
+
+a =int(input("enter first number:"))
+b =int(input("enter second number:"))
+
+# # arithmetic operations
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# comparison operators
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater Than:", a > b)
+print("Less Than:", a < b)
+print("Greater Than or Equal:", a >= b)
+print("Less Than or Equal:", a <= b)
+
+# # logical operators
+print("AND:", a and b)
+print("OR:", a or b)
+print("NOT:", not a)
