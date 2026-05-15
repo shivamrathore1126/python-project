622ade038604455d26f54b4aa4678c8fc7188be8
Shivam Rathore <shivamrathore1126@gmail.com>
2026-05-15 19:12:37 +0530

    strings in python

---
+++ 07_string.py
@@ -0,0 +1,22 @@
+# string = used to store text data
+name = "shivam"
+print(name)
+
+# multiline string = used to write text in multiple lines
+message = '''
+Hi Shivam
+How are you
+I am learning python
+'''
+print(message)
+
+# accessing characters of string = access letters using index number
+name = "apple"
+print(name[0])   # first character
+print(name[1])   # second character
+print(name[2])   # third character
+
+
+# looping through string = print all characters one by one
+for character in name:
+    print(character)
